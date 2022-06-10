2a366cc0fcac05bf26d73b521498182d912fb015
sukhpreetsinghsidhu <sukhpreetsinghsidhu@hotmail.com>
2022-06-09 22:20:52 -0400

    Working meme repo
    create custom memes
    browse online memes
    
    home button on all pages
    extensively commented code

--- app/src/main/java/com/example/memeoroid/MainActivity.kt
+++ app/src/main/java/com/example/memeoroid/MainActivity.kt
@@ -3,27 +3,40 @@ package com.example.memeoroid
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Toast
 import kotlinx.android.synthetic.main.activity_main.*
 
+//Home Page/ Main Activity page
+//Navigates to the different functionalities and pages of the app
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
+        //Hides the app title and the system notifications top bar
+        supportActionBar?.hide()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
 
-        createMeme.setOnClickListener(){
-            var intent = Intent(this, NewActivity::class.java)
+        //Redirect to the new meme creation page
+        createMeme.setOnClickListener{
+            val intent = Intent(this, NewActivity::class.java)
             startActivity(intent)
+            Toast.makeText(applicationContext,"Redirecting to Create Meme", Toast.LENGTH_LONG).show()
         }
 
-        favorites.setOnClickListener(){
-            var intent = Intent(this, FavoritesActivity::class.java)
+        //Redirect to the favorite meme list page
+        favorites.setOnClickListener{
+            val intent = Intent(this, FavoritesActivity::class.java)
             startActivity(intent)
+            Toast.makeText(applicationContext,"Redirecting to Favorite Memes", Toast.LENGTH_LONG).show()
         }
 
-        surfMeme.setOnClickListener(){
-            var intent = Intent(this, SurfMemesActivity::class.java)
+        //Redirect to the surf memes randomly selected and display from reddit
+        surfMeme.setOnClickListener{
+            val intent = Intent(this, SurfMemesActivity::class.java)
             startActivity(intent)
+            Toast.makeText(applicationContext,"Redirecting to Browse Memes", Toast.LENGTH_LONG).show()
         }
     }
 }
--- app/src/main/java/com/example/memeoroid/SurfMemesActivity.kt
+++ app/src/main/java/com/example/memeoroid/SurfMemesActivity.kt
@@ -10,54 +10,58 @@ import com.android.volley.Request
 import com.android.volley.Response
 import com.android.volley.toolbox.JsonObjectRequest
 import com.android.volley.toolbox.Volley
+import com.google.android.material.floatingactionbutton.FloatingActionButton
 import com.squareup.picasso.Picasso
 import kotlinx.android.synthetic.main.activity_surfmemes.*
 
+//Browse Randoms memes from the internet
 class SurfMemesActivity : AppCompatActivity() {
 
-    lateinit var memeImageView : ImageView
-
-    var currentImageURL : String? = null
+    //Imageview component to load and display random meme
+    private lateinit var memeImageView : ImageView
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_surfmemes)
 
+        //Hides the app title and the system notifications top bar
+        supportActionBar?.hide()
+        window.decorView.systemUiVisibility = View.SYSTEM_UI_FLAG_FULLSCREEN
+
+        //Floating button redirects to the home page
+        val floatingBtn : FloatingActionButton = findViewById(R.id.floatingBtn)
+
         memeImageView = findViewById(R.id.memeImageView)
 
-        loadMeme()
-        progressBar.visibility = View.INVISIBLE
+        //Function loads new meme on page creation
+        loadNewMeme()
 
+        //Button listener to load a new meme on click
         nextButton.setOnClickListener{
-            loadMeme()
+            loadNewMeme()
+            Toast.makeText(applicationContext,"New Meme", Toast.LENGTH_SHORT).show()
         }
 
-        shareButton.setOnClickListener{
-            val intent = Intent(Intent.ACTION_SEND)
-            intent.type = "text/plain"
-            intent.putExtra(Intent.EXTRA_TEXT,"Checkout this meme $currentImageURL")
-            val chooser = Intent.createChooser(intent,"Share this meme using- ")
-            startActivity(chooser)
+        floatingBtn.setOnClickListener{
+            val myIntent = Intent(this, MainActivity::class.java)
+            startActivity(myIntent)
+            Toast.makeText(applicationContext,"Redirecting to Home Page", Toast.LENGTH_LONG).show()
         }
     }
 
-    fun loadMeme(){
-
+    //function to load a new image into the imageview
+    //Uses Volley to fetch and load images
+    //Volley because it has inbuilt support for images and this page is only to fetch and display images
+    private fun loadNewMeme(){
         val queue = Volley.newRequestQueue(this)
         val url = "https://meme-api.herokuapp.com/gimme"
+        var currentImageURL : String?
 
-        val jsonObjectRequest = JsonObjectRequest(
-            Request.Method.GET, url,null,
-            Response.Listener{ response ->
-                currentImageURL = response.getString("url")
-
-                Picasso.with(this@SurfMemesActivity).load(currentImageURL).into(memeImageView)
-                //Glide.with(this).load(currentImageURL).into(memeImageView)
-                //println("in the response listnener")
-            },
+        val jsonObjectRequest = JsonObjectRequest(Request.Method.GET, url,null,
+            Response.Listener{ response -> currentImageURL = response.getString("url")
+            Picasso.with(this@SurfMemesActivity).load(currentImageURL).into(memeImageView)},
             Response.ErrorListener {
-                Toast.makeText(this,"Something went wrong", Toast.LENGTH_LONG).show()
-            })
+            Toast.makeText(this,"Something went wrong", Toast.LENGTH_LONG).show() })
         queue.add(jsonObjectRequest)
     }
 }
