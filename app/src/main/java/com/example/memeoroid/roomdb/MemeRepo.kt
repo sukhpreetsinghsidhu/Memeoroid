1badf8668b8516a3e9233cc05c747a0b4cb6187a
jinal.parmar@greylabs.com <jinalparmar45@gmail.com>
2022-06-10 16:34:51 -0400

    testcases for favourites

--- app/src/main/java/com/example/memeoroid/roomdb/MemeRepo.kt
+++ app/src/main/java/com/example/memeoroid/roomdb/MemeRepo.kt
@@ -6,38 +6,38 @@ import androidx.lifecycle.LiveData
 // DB Data Repository
 // Near identical to the DAO
 // Like the DbViewModel, its job is to separate the data from the UI
-class MemeRepo(context: Context) {
-    var db:MemeDao? = AppDatabase.getInstance(context)?.memeDao()
+class MemeRepo(var dao : MemeDao ){ //context: Context) {
+    //var db:MemeDao? = AppDatabase.getInstance(context)?.memeDao()
 
     // Create
     fun insertFavorite(meme: Meme) {
-        db?.insertFavorite(meme)
+        dao?.insertFavorite(meme)
     }
 
     // Read
     fun selectAllFavorites(limit: Int, offset: Int) : LiveData<List<Meme>>? {
-        return db?.selectAllFavorites(limit, offset)
+        return dao?.selectAllFavorites(limit, offset)
     }
 
     fun selectFavorite(key: Int, limit :Int, offset: Int): LiveData<Meme>? {
-        return db?.selectFavorite(key, limit , offset)
+        return dao?.selectFavorite(key, limit , offset)
     }
 
     fun search(text: String, limit:Int, offset : Int) : LiveData<List<Meme>>? {
-        return db?.search("%" + text + "%", limit, offset )
+        return dao?.search("%" + text + "%", limit, offset )
     }
 
     // Update
     fun updateFavorite(meme: Meme) {
-        db?.updateFavorite(meme)
+        dao?.updateFavorite(meme)
     }
 
     // Delete
     fun deleteFavorite(meme: Meme) {
-        db?.deleteFavorite(meme)
+        dao?.deleteFavorite(meme)
     }
 
     fun deleteAllFavorites() {
-        db?.deleteAllFavorites()
+        dao?.deleteAllFavorites()
     }
 }
---
+++ app/src/test/java/com/example/memeoroid/FavouritesTest.kt
@@ -0,0 +1,85 @@
+package com.example.memeoroid
+
+import android.content.Context
+import androidx.lifecycle.MutableLiveData
+import androidx.room.Room
+import androidx.test.core.app.ApplicationProvider
+import com.example.memeoroid.retrofit.RetroApiInterface
+import com.example.memeoroid.roomdb.AppDatabase
+import com.example.memeoroid.roomdb.Meme
+import com.example.memeoroid.roomdb.MemeDao
+import com.example.memeoroid.roomdb.MemeRepo
+import org.junit.Assert
+import org.junit.Before
+import org.junit.Test
+import org.junit.runner.RunWith
+import org.junit.runners.JUnit4
+import org.mockito.Mock
+import org.mockito.Mockito
+import org.mockito.MockitoAnnotations
+
+@RunWith(JUnit4::class)
+class FavouritesTest {
+
+    @Mock
+    lateinit var dao: MemeDao
+
+    @Mock
+    lateinit var inter: RetroApiInterface
+
+    @Mock
+    lateinit var AppDatabase : AppDatabase
+    @Mock
+    lateinit var repo : MemeRepo
+    @Before()
+    fun setup(){
+        MockitoAnnotations.openMocks(this)
+       // val context = ApplicationProvider.getApplicationContext<Context>()
+//        val db = Room.inMemoryDatabaseBuilder(
+//            context, AppDatabase::class.java).build()
+//        dao = db.memeDao();
+
+        repo = MemeRepo(dao)
+
+    }
+
+    @Test
+    fun TestgetallMeme(){
+        var fakeList: List<Meme> = listOf<Meme>(Meme(3,"top", "bottom","img1"))
+
+        // mock the function call to the api
+        Mockito.`when`(dao.selectAllFavorites(1,0))
+            .thenReturn(MutableLiveData(fakeList))
+        var result = repo.selectAllFavorites(1,0)
+        Assert.assertEquals(result?.value ,fakeList)
+
+
+    }
+
+    @Test
+    fun TestselectFavorite(){
+        var fakeList =Meme(45,"top", "bottom","img1")
+
+        // mock the function call to the api
+        Mockito.`when`(dao.selectFavorite(45,1,0))
+            .thenReturn(MutableLiveData(fakeList))
+        var result = repo.selectFavorite(45,1,0)
+
+        Assert.assertEquals(fakeList , result?.value )
+
+
+    }
+
+    //search
+    @Test
+    fun Testsearch(){
+        var fakeList =listOf<Meme>(Meme(45,"top", "bottom","img1"))
+
+        // mock the function call to the api
+        Mockito.`when`(dao.search("%to%",1,0))
+            .thenReturn(MutableLiveData(fakeList))
+        var result = repo.search("to",1,0)
+
+        Assert.assertEquals(fakeList , result?.value )
+    }
+}
