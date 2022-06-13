8836cca008bfa73788cd684a9e2a0dc01306e592
jinalparmar45 <86338136+jinalparmar45@users.noreply.github.com>
2022-06-13 15:46:21 -0400

    Revert "John animation"

--- app/src/main/java/com/example/memeoroid/roomdb/DbViewModel.kt
+++ app/src/main/java/com/example/memeoroid/roomdb/DbViewModel.kt
@@ -18,9 +18,7 @@ class DbViewModel(app: Application) : AndroidViewModel(app) {
     init {
         val dao = AppDatabase.getInstance(app)?.memeDao()
         repo = MemeRepo(dao!!)
-
         allFavorites = repo.selectAllFavorites(limit,0)!!
-
     }
 
     // Create
--- app/src/main/java/com/example/memeoroid/roomdb/OnSwipeLeft.kt
+++ app/src/main/java/com/example/memeoroid/roomdb/OnSwipeLeft.kt
@@ -2,15 +2,10 @@ package com.example.memeoroid.roomdb
 
 import android.content.Context
 import android.graphics.Canvas
-import androidx.core.content.ContextCompat
 import androidx.recyclerview.widget.ItemTouchHelper
 import androidx.recyclerview.widget.RecyclerView
-import com.example.memeoroid.R
 
 abstract class OnSwipeLeft(context: Context): ItemTouchHelper.SimpleCallback(0, ItemTouchHelper.LEFT){
-
-    val updateColor = ContextCompat.getColor(context, R.color.updateColor)
-    val updateIcon = R.drawable.ic_baseline_build_circle_24
 
     override fun onMove(
         recyclerView: RecyclerView,
@@ -29,13 +24,6 @@ abstract class OnSwipeLeft(context: Context): ItemTouchHelper.SimpleCallback(0, 
         actionState: Int,
         isCurrentlyActive: Boolean
     ) {
-//        RecyclerViewSwipeDecorator.Builder()
-//        RecyclerViewSwipeDecorator.Builder(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
-//            .addSwipteLeftBackgroundColor(updateColor)
-//            .addSwipeLeftActionIcon(updateIcon)
-//            .create()
-//            .decorate()
-
         super.onChildDraw(c, recyclerView, viewHolder, dX, dY, actionState, isCurrentlyActive)
     }
 }
